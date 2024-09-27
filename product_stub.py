2af2e72ec62ac09eeb0caa02a5f512275ecac95a
Muhammad Ibtihaj <m.ibtihaj23@gmail.com>
2024-09-27 20:13:03 +0500

    Removed separate file and made it into primary stub File

--- product_stub.py
+++ product_stub.py
@@ -1,14 +1,24 @@
 class Product:
-  def __init__(self, name, amount, price):
+    def __init__(self, name, amount, price):
       self.name = name
       self.amount = amount
       self.price = price
 
-  def get_price(self, quantity):
-      pass
+    def get_price(self, quantity):
+        if quantity <= 0:
+            raise ValueError("Quantity can't be Zero!")
+        price = self.price * quantity
+        if quantity >= 10:
+            discount = price * (10/100)
+            price = price - discount
+            return price
 
-  def make_purchase(self, quantity):
-      pass
+        elif quantity >= 100:
+            discount = price * (20/100)
+            price = price - discount
+            return price
+
+
 
 # create product object
 # make purchases against different product quantities (make sure to run each test case)
